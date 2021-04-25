91846c8b7f38e2a77d1de34e6e13eb30eb8958dd
nasfernane <nasfernane@gmail.com>
2021-04-25 16:17:54 +0200

    minor fix

--- app/Models/Anime.php
+++ app/Models/Anime.php
@@ -5,7 +5,6 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-use Illuminate\Support\Facades\DB;
 
 class Anime extends Model
 {
@@ -13,5 +12,4 @@ class Anime extends Model
 
     protected $table = 'animes';
 
-    
 }
